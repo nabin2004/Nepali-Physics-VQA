6d6684dcf8f4ba0c5a8e446552a46e3f99ac4863
nabin2004 <olinabin2004@gmail.com>
2025-08-13 09:51:45 +0545

    Added exception code

--- packages/llmops/main.py
+++ packages/llmops/main.py
@@ -1,5 +1,14 @@
 from llmops.logging.logger import logging
+from llmops.exception.exception import LLMOpsException
+import sys 
 
-logging.info("Fine-tuning started")
-logging.warning("Dataset size is small, results may vary")
-logging.error("Training failed due to GPU OOM")
+try:
+    logging.info("Fine-tuning started")
+    logging.warning("Dataset size is small, results may vary")
+
+    # Error simulation
+    1 / 0
+
+except Exception as e:
+    logging.error("Training failed due to GPU OOM or other error")
+    raise LLMOpsException(e, sys)
