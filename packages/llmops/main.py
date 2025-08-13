e1794018e51cb609a0d5d237e44251b4c4fd7475
nabin2004 <olinabin2004@gmail.com>
2025-08-13 09:41:16 +0545

    Added logger for LLMOps

---
+++ packages/llmops/main.py
@@ -0,0 +1,5 @@
+from llmops.logging.logger import logging
+
+logging.info("Fine-tuning started")
+logging.warning("Dataset size is small, results may vary")
+logging.error("Training failed due to GPU OOM")
